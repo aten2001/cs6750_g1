de802aeea45cbb3a117dcc3a5bc17a8202593f19
Leonard Ramsey <lr3hj@virginia.edu>
2018-03-13 17:48:13 -0400

    Added home page/url/view.

--- cs6750_g1/app/views.py
+++ cs6750_g1/app/views.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+
+def index(request):
+    return render(request, 'app/index.html', {})
