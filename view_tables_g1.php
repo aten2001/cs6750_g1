107c0789c252b3d47d996d8b354c319d3a96dc15
Leonard Ramsey <lr3hj@virginia.edu>
2018-04-23 15:07:41 -0400

    Updated view tables page to show all items currently in the database.

--- view_tables_g1.php
+++ view_tables_g1.php
@@ -78,82 +78,51 @@ $inputUsername = $inputPassword = $pwd= "";
       <a href="update_inventory_g1.php">Inventory</a>
       <a class="active" href="view_tables_g1.php">View Tables</a>
   </div> 
-<h2 style="text-align: center; 
-  font-family: 'Josefin Sans', cursive;
-  display: block;
-	color: #000066;
-	font-size: 58px;
-	font-weight: bold;"> View Records </h2>
-<hr> 
-<h2 style="text-align: center; font-family: 'Josefin Sans', cursive; color: #FF6600;font-size: 30px;"> Operations Here</h2>
-<hr>
+  <div class="content" style="padding-left: 20px; padding-right: 20px;">
+    <h1 style="text-align: left; 
+        display: block;
+        color: #000066;
+        font-weight: bold;">View Records</h1>
+      <hr> 
+      <b style="text-align: left; font-size: 18px;">The table below consists of all items currently in the database.</b>
+      <hr>
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Category</td>
+          <td>Price</td>
+          <td>Inventory</td>
+          <td>Discount Rate</td>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+                mysql_select_db("lr3hj");
+                $sql = "Select * from Items";
+                $results = $conn->query($sql);
+                while($row = $results->fetch_assoc()) {
+                ?>
+                    <tr>
+                        <td><?php echo $row['ItemID']?></td>
+                        <td><?php echo $row['ItemName']?></td>
+                        <td><?php echo $row['CategoryID']?></td>
+                        <td><?php echo $row['Price']?></td>
+                        <td><?php echo $row['Inventory']?></td>
+                        <?php if ($row['DiscountRate'] == NULL) { ?>
+                        <td><?php echo "None"?></td>
+                        <?php } else { ?>
+                        <td><?php echo $row['DiscountRate']?></td>
+                        <?php } ?>
+                    </tr>
 
-<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-
-  Name:
-  <br>
-  <input type="text" name="inputUsername" value="<?php echo $inputUsername;?>">
-  <span class="error"> <?php echo $nameErr;?></span>
-  <br>
-
-  Password:
-  <br>
-  <input type="text" name="inputPassword" value="<?php echo $inputPassword;?>">
-  <span class="error"> <?php echo $passErr;?></span>
-  <br>
-
-  <br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-{
-	$inputUsername = $_POST["inputUsername"];
-	$inputPassword = $_POST["inputPassword"];
-	//echo $inputUsername . "<br>";
-
-}
-
-
-//$sql = "Select * from Login WHERE username = \"" . $inputUsername . "\"";  
-// HINT: your SQL Query to get the row with given username
-// Is it Select or Insert or Delete or Alter? 
-
-$sql = "Select OrderID from Orders WHERE TrackingNumber > 500";
-
-
-$result = $conn->query($sql);
-
-
-if ($result->num_rows > 0) 
-{
-	
-    // output data of each row -- here we have one or 0 rows because username is primary key
-    while($row = $result->fetch_assoc()) 
-    {
-        $str = $row["OrderID"] . "\n";
-        echo $str;
-    }
-} 
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
-  if($pwd == $inputPassword)
-    //HINT: Which variable did you use to store your password obtained from the SQL query 
-  {
-  echo "Connection success!";
-  // you can add code to jump to welcome page
-  }
-  else
-  {
-  echo "Password or username is incorrect";
-  }
-}
-
-
-?>
+                <?php
+                }
+        ?>
+      </tbody>
+    </table>
+</div>
 
 
 
